6b94b16118d6f3c84088aa7363452fd0852fae92
vat99 <talwar_varun@bah.com>
2024-02-29 00:14:22 -0500

    added initial structure

---
+++ parse/pdf_reader.py
@@ -0,0 +1,10 @@
+import fitz
+
+def get_fitz_doc_obj(f_name):
+    doc = fitz.open(f_name)
+    if doc.page_count < 1:
+        doc.close()
+        raise PageCountParse(
+            f"Could not parse the doc, failed page count check: {f_name}")
+    else:
+        return doc
