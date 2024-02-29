0233c6f598711a1dd158cb93c073e23725ad5d69
vat99 <talwar_varun@bah.com>
2024-02-29 04:06:55 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name="gc-parse", version="0.1", packages=find_packages(where="parse"))
+setup(
+    name="parse",
+    version="0.1",
+    packages=["parse"],
+    url="https://github.com/vat99/parser",
+)
